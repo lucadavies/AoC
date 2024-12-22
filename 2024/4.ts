72e2fcc9f52873f7644ccd817d0144dda862b2a0
Luca Davies <luca-davies@outlook.com>
2024-12-22 19:59:47 +0000

    Tidying and commets

--- 2024/4.ts
+++ 2024/4.ts
@@ -16,13 +16,6 @@ enum Direction {
     DownRight
 }
 
-enum Rotation {
-    "0d",
-    "90d",
-    "180d",
-    "270d"
-}
-
 export function main(): void {
     mainT1();
     mainT2();
@@ -30,10 +23,13 @@ export function main(): void {
 
 function mainT1(): void {
     count = 0;
+    // Iterate over every letter in the grid
     for (let j = 0; j < lines[0].length; j++) {
         for (let i = 0; i < lines.length; i++) {
+            // Target letter to begin with
             let k = 0;
             if (lines[j][i] === target[k]) {
+                // Check each direction recursively along the target word
                 findNextChar(j, i, k + 1, Direction.Up);
                 findNextChar(j, i, k + 1, Direction.Down);
                 findNextChar(j, i, k + 1, Direction.Left);
@@ -50,6 +46,7 @@ function mainT1(): void {
 
 function mainT2(): void {
     count = 0;
+    // Iterate over every letter in the grid, ignoring the edges as centre of pattern cannot be at the edge.
     for (let j = 1; j < lines[0].length - 1; j++) {
         for (let i = 1; i < lines.length - 1; i++) {
             if (lines[j][i] === "A")
@@ -61,6 +58,13 @@ function mainT2(): void {
     console.log("X-MAS Count: " + count);
 }
 
+/**
+ * Recursive function to check presence of target word in input. Adds 1 to count if end of target word is reached.
+ * @param y y coord of letter we're checking
+ * @param x x coord of letter we're checking
+ * @param z index of letter in target word
+ * @param dir direction we're checking
+ */
 function findNextChar(y: number, x: number, z: number, dir: Direction): void {
     if (z < target.length) {
         switch (dir) {
@@ -135,22 +139,39 @@ function findNextChar(y: number, x: number, z: number, dir: Direction): void {
     }
 }
 
+/**
+ * A not-very-generic function to check for presence of our cross-pattern.
+ * @param y y coord of centre letter in our cross pattern (A)
+ * @param x x coord of centre letter in our cross pattern (A)
+ */
 function findCrossMAS(y: number, x: number): void {
+    /** Check for MS pattern (shown visually in IFs)
+     *            MS
+     */
     if (lines[y - 1][x - 1] === "M" && lines[y - 1][x + 1] === "S" ) {
         if (lines[y + 1][x - 1] === "M" && lines[y + 1][x + 1] === "S") {
             count++;
         }
     }
+    /** Check for MS pattern (shown visually in IFs)
+     *            SS
+     */
     else if (lines[y - 1][x - 1] === "M" && lines[y - 1][x + 1] === "M" ) {
         if (lines[y + 1][x - 1] === "S" && lines[y + 1][x + 1] === "S") {
             count++;
         }
     }
+    /** Check for SM pattern (shown visually in IFs)
+     *            SM
+     */
     else if (lines[y - 1][x - 1] === "S" && lines[y - 1][x + 1] === "M" ) {
         if (lines[y + 1][x - 1] === "S" && lines[y + 1][x + 1] === "M") {
             count++;
         }
     }
+    /** Check for SS pattern (shown visually in IFs)
+     *            MM
+     */
     else if (lines[y - 1][x - 1] === "S" && lines[y - 1][x + 1] === "S" ) {
         if (lines[y + 1][x - 1] === "M" && lines[y + 1][x + 1] === "M") {
             count++;
