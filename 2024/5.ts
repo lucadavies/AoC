4b3003d71634b3a164f8e04ccbd08111a60a2aba
Luca Davies <luca-davies@outlook.com>
2024-12-23 00:21:35 +0000

    Day 5 task 2 (definitely not optimised)

--- 2024/5.ts
+++ 2024/5.ts
@@ -2,29 +2,35 @@ import { getFileLines } from "./ReadFileLines";
 
 let fileName: string = "5.txt";
 let lines: string[] = getFileLines(fileName);
-let total: number = 0;
-let incorrectIndex: number[] = [];
 
 export function main(): void {
     let rules: number[][] = [];
     let updates: number[][] = [];
+    let correctedUpdates: number[][] = [];
+    let correctIndexes: number[] = [];
+    let incorrectIndexes: number[] = [];
 
     getInputSections(rules, updates);
 
-    for (let update of updates) {
-        //console.log("Update index: " + updates.indexOf(update));
-        if (isCorrect(update, rules)) {
-            //console.log("Correct");
-            total += update[(update.length - 1) / 2];
+    for (let i = 0; i < updates.length; i++) {
+        if (isCorrect(updates[i], rules)) {
+            correctIndexes.push(i);
+        }
+        else {
+            incorrectIndexes.push(i);
         }
     }
-    console.log("Total: " + total);
+
+    correctUpdates(updates, incorrectIndexes, rules);
+
+    console.log("Total for correct: " + getSumMiddlePages(updates, correctIndexes));
+    console.log("Total for incorrect: " + getSumMiddlePages(updates, incorrectIndexes));
 }
 
 /**
- * Parses the input to acquire the two separate input sections, separated a blank line. Parses both rules and updates section to numbers.
- * @param a Array to push rules input section to
- * @param b Array to push updates input section to
+ * Parses the input to acquire the two separate input sections, separated a blank line. Parses both rules and updates section to numbers..
+ * @param a Array to push rules input section to.
+ * @param b Array to push updates input section to.
  */
 function getInputSections(a: number[][], b: number[][]): void {
     let isRules = true;
@@ -45,6 +51,12 @@ function getInputSections(a: number[][], b: number[][]): void {
     }
 }
 
+/**
+ * Returns true is seq passes the given rules.
+ * @param seq Sequence of numbers to check against rules.
+ * @param rules The set of rules to check seq against.
+ * @returns True is seq obeys the rules, else returns false.
+ */
 function isCorrect(seq: number[], rules: number[][]): boolean {
     let foundIndex = 0;
     // Iterate over each page in update
@@ -55,13 +67,58 @@ function isCorrect(seq: number[], rules: number[][]): boolean {
                 // Get index of second page of rule (this must be after first)
                 foundIndex = seq.findIndex(x => x === rule[1])
                 // If second page is first, return false
-                if (foundIndex >=0 && foundIndex < i) {
-                    // console.log("Failed on " + seq[i] + " with rule " + rule[0] + "|" + rule[1]);
-                    
+                if (foundIndex >= 0 && foundIndex < i) {
                     return false;
                 }
             }
         }
     }
     return true;
+}
+
+/**
+ * Performs the same function as isCorrect, but reorders elements to satisfy rules as it goes.
+ * @param seq Sequence of numbers to check against rules.
+ * @param rules The set of rules to check seq against.
+ */
+function correctUpdates(updates: number[][], indexes: number[], rules: number[][]): void {
+    for (let i of indexes) {
+        let foundIndex = 0;
+        let page;
+        // Iterate over each page in update
+        for (let j = 1; j < updates[i].length; j++) {
+            page = updates[i][j]
+            for (let rule of rules) {
+                // If rule exists for that page
+                if (rule[0] === page) {
+                    // Get index of second page of rule (this must be after first)
+                    foundIndex = updates[i].findIndex(x => x === rule[1])
+                    // If second page is first, return false
+                    if (foundIndex >= 0 && foundIndex < j) {
+                        // Swap pages that are out of order
+                        updates[i].splice(foundIndex, 0, updates[i].splice(j, 1)[0]);
+                        j--;
+                    }
+                }
+            }
+        }
+
+        // Check again if updates are correct, and if not, run another pass of correction again.
+        if (!isCorrect(updates[i], rules)) {
+            correctUpdates(updates, indexes, rules);
+        } 
+    }
+}
+
+/**
+ * Sums the middle value in each update defined by the indexes passed in.
+ * @param updates The array updates from which to find and sum middle values.
+ * @param indexesToSum The indexes of the updates to sum.
+ */
+function getSumMiddlePages(updates: number[][], indexesToSum: number[]): number {
+    let total: number = 0;
+    for (let i of indexesToSum) {
+        total += updates[i][(updates[i].length - 1) / 2];
+    }
+    return total;
 }
